3a610426c2dd59879a04c830632541972ea1e06c
truthless-dev <truthless.deviant248@simplelogin.fr>
2026-02-10 12:24:34 -0800

    (rust/anagram) iteration 4
    Utilize `.copied()` instead of manually copying via `.map()`.
    
    Reduce unnecessary heap allocation by
    
    * First checking the lengths of the target word and each candidate.
    * Moving the lowercasing out of the helper `count_graphemes()`.
    
    Cutting down on calls to `.to_lowercase()` means fewer allocations on
    the heap and thus a more efficient program. See the last three notes in
    the mentor discussion for these points.
    
    Note that the signature for `count_graphemes()` also had to change to
    conform to the new strategy. Instead of returning
    `HashMap<String, u32>`, it now returns `HashMap<&str, u32>`. This is
    due to the removal of `.to_lowercase()` from this function. It now
    simply stores references to the graphemes instead of allocating com-
    pletely new strings.

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -4,10 +4,10 @@ use unicode_segmentation::UnicodeSegmentation;
 /// Construct a mapping of graphemes to integers such that each
 /// integer value is a count of how many times the grapheme appears
 /// in the given string slice.
-fn count_graphemes(s: &str) -> HashMap<String, u32> {
+fn count_graphemes(s: &str) -> HashMap<&str, u32> {
     s.graphemes(true)
         .fold(HashMap::new(), |mut accumulator, grapheme| {
-            let count = accumulator.entry(grapheme.to_lowercase()).or_insert(0);
+            let count = accumulator.entry(grapheme).or_insert(0);
             *count += 1;
             accumulator
         })
@@ -16,16 +16,28 @@ fn count_graphemes(s: &str) -> HashMap<String, u32> {
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let word_counts = count_graphemes(word);
+    let word_lower = word.to_lowercase();
+    let word_counts = count_graphemes(&word_lower);
     possible_anagrams
         .iter()
         .filter(|s| {
-            if s.to_lowercase() == word.to_lowercase() {
+            if s.len() != word_lower.len() {
+                // If the strings are of different lengths, they cannot
+                // be anagrams.
+                // We check this before lowercasing `s` in order to
+                // avoid unnecessary memory allocations.
                 return false;
             }
-            let possible_counts = count_graphemes(s);
-            word_counts == possible_counts
+
+            let s_lower = s.to_lowercase();
+            if s_lower == word_lower {
+                // A word is not an anagram of itself.
+                return false;
+            }
+
+            let s_counts = count_graphemes(&s_lower);
+            s_counts == word_counts
         })
-        .map(|s| *s)
+        .copied()
         .collect()
 }
