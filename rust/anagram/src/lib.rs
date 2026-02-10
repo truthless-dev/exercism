6e0bf0eb09c157f92520dfca8071f65c01fbdf2d
truthless-dev <truthless.deviant248@simplelogin.fr>
2026-02-10 12:04:53 -0800

    (rust/anagram) iteration 3
    Use `.fold()` instead of a for-loop to count the graphemes in a
    string (see note 2 in the mentor discussion).

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,13 +1,18 @@
 use std::collections::{HashMap, HashSet};
 use unicode_segmentation::UnicodeSegmentation;
 
+/// Construct a mapping of graphemes to integers such that each
+/// integer value is a count of how many times the grapheme appears
+/// in the given string slice.
 fn count_graphemes(s: &str) -> HashMap<String, u32> {
-    let mut counts = HashMap::new();
-    for g in s.graphemes(true) {
-        let count = counts.entry(g.to_lowercase()).or_insert(0u32);
-        *count += 1;
-    }
-    counts
+    s.graphemes(true)
+        .fold(HashMap::new(), |mut accumulator, grapheme| {
+            let count = accumulator.entry(grapheme.to_lowercase()).or_insert(0);
+            *count += 1;
+            accumulator
+        })
+    // `.fold()` returns the final value of `accumulator`, not another
+    // iterator, so we can simply return its return value.
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
