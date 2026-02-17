4970628b6919869f8b0d47a77cec28ed49e2f3c1
truthless-dev <truthless.deviant248@simplelogin.fr>
2026-02-17 01:17:16 -0800

    (rust/acronym) iteration 2
    Switch to a state machine implementation instead of relying on multiple
    separate iterators with helper functions in order to improve the
    program's efficiency. Using this algorithm means each character in the
    input is checked only once, and there is only one allocation (for the
    output string). While other fully-iterator-based implementations can
    potentially achieve similar efficiency, they are arguably more difficult
    to reason about.

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,34 +1,31 @@
-/// Determine whether a string contains only uppercase chars.
-fn is_uppercase(word: &str) -> bool {
-    word.chars().all(|c| c.is_uppercase())
-}
-
-/// Split a string by spaces and hyphens.
-fn get_words(s: &str) -> Vec<&str> {
-    s.split(['-', ' '].as_slice()).collect()
-}
-
 pub fn abbreviate(phrase: &str) -> String {
     let mut acronym = String::new();
-    for word in get_words(phrase) {
-        let mut iter_chars = word.chars();
 
-        // Find the word's first alphabetic character.
-        for c in iter_chars.by_ref() {
+    let mut in_word = false;
+    let mut is_camel_case = false;
+
+    for c in phrase.chars() {
+        if in_word {
+            if !c.is_alphabetic() && c != '\'' {
+                // Reached word boundary (apostrophe does not break a word).
+                in_word = false;
+                is_camel_case = false;
+            } else if c.is_lowercase() && !is_camel_case {
+                is_camel_case = true;
+            } else if c.is_uppercase() && is_camel_case {
+                acronym.push(c);
+            }
+        } else {
             if c.is_alphabetic() {
-                c.to_uppercase().for_each(|c| acronym.push(c));
-                break;
+                in_word = true;
+                if c.is_lowercase() {
+                    is_camel_case = true;
+                    c.to_uppercase().for_each(|c| acronym.push(c));
+                } else if c.is_uppercase() {
+                    acronym.push(c);
+                }
             }
         }
-
-        if is_uppercase(word) {
-            continue;
-        }
-
-        // If the word is camel-cased, add each uppercase char.
-        iter_chars
-            .filter(|c| c.is_uppercase())
-            .for_each(|c| acronym.push(c));
     }
 
     acronym
